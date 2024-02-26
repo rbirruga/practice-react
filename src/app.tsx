0d3458bb4cb1eb6a52c57ba7484284c4d319d695
Rodrigo Dulizio <rodrigo.dulizio@gmail.com>
2024-02-25 22:08:20 -0300

    feat: add new tag

--- src/app.tsx
+++ src/app.tsx
@@ -7,8 +7,9 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from ".
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { Pagination } from "./components/pagination";
 import { useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import useDebounceValue from "./hooks/use-debounce-value";
+import { useState } from "react";
+import * as Dialog from '@radix-ui/react-dialog';
+import { CreateTagForm } from "./components/create-tag-form";
 
 export interface TagResponse {
   first: number
@@ -23,6 +24,7 @@ export interface TagResponse {
 export interface Tag {
   id: string
   title: string
+  slug: string
   amountOfVideos: number
 }
 
@@ -32,17 +34,7 @@ export function App() {
   
   const [filter, setFilter] = useState(urlFilter);
 
-  const debouncedFilter = useDebounceValue(filter, 700);
-
   const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1;
-
-  useEffect(() => {
-    setSearchParams(params => {
-      params.set('page', '1');
-
-      return params;
-    })
-  }, [debouncedFilter, setSearchParams]);
 
   const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
     queryKey: ['get-tags', urlFilter, page],
@@ -81,10 +73,33 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
         <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant="primary">
-            <Plus className="size-3" />
-            Create new
-          </Button>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary">
+                <Plus className="size-3" />
+                Create new
+              </Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="fixed inset-0 bg-black/70"/>
+              <Dialog.Content className="fixed space-y-10 p-10 right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-950 border-l border-zinc-800">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold">
+                    Create Tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-500">
+                    Tags can be used to group-videos about similar concepts
+                  </Dialog.Description>
+                </div>
+
+                <CreateTagForm />
+
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+
+          </Dialog.Root>
         </div>
 
         <div className="flex items-center justify-between">
@@ -128,7 +143,7 @@ export function App() {
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
                       <span className="font-medium">{tag.title}</span>
-                      <span className="text-xs text-zinc-400">{tag.id}</span>
+                      <span className="text-xs text-zinc-400">{tag.slug}</span>
                     </div>
                   </TableCell>
                   <TableCell className="text-zinc-300">
